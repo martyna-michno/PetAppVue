d221b3e98210e356f902bd16b22adcc1b0d3af01
Martyna Michno <mjjmichno@gmail.com>
2020-01-21 08:54:29 +0100

    working on form

--- adopt-pets/src/views/Home.vue
+++ adopt-pets/src/views/Home.vue
@@ -2,6 +2,39 @@
   <div class="home">
 <h1>Adopt a new best friend.</h1>
 <button class="btn btn-primary">Add New Pet</button>
+<b-form @submit="handleSubmit"  v-if="showPetForm">
+      <b-form-group id="input-group-2" label="Pet's Name:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="form.name"
+          required
+          type="text"
+          placeholder="Enter name"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-3" label="Species:" label-for="input-3">
+        <b-form-select
+          id="input-3"
+          v-model="form.food"
+          :options="['cats', 'dogs']"
+          required
+        ></b-form-select>
+      </b-form-group>
+
+ <b-form-group id="input-group-2" label="Pet's Age:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="form.name"
+          required
+          type="number"
+          placeholder="Enter age"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-button type="submit" variant="primary">Submit</b-button>
+      <b-button type="reset" variant="danger">Reset</b-button>
+    </b-form>
   </div>
 </template>
 
@@ -11,12 +44,20 @@ export default {
   name: 'home',
   data () {
     return {
-      showPetForm: false
+      showPetForm: false,
+      formData: {
+        name: '',
+        age: 0,
+        species: null
+      }
     }
   },
   methods: {
     togglePetForm () {
       this.showPetForm = !this.showPetForm
+    },
+    handleSubmit () {
+
     }
   }
 }
